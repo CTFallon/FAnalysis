c130f4b9a6dd548906be9f86ec6506165d3ae13f
CTFallon <c.fallon@cern.ch>
2018-09-05 10:11:39 -0500

    First Test commit

---
+++ analysisBase.py
@@ -0,0 +1,131 @@
+# base class for FAnalysis, copied from Lucien Lo's PFG HcalTupleAnalyzer, adapted to python
+import ROOT as rt
+class baseClass():
+	def __init__(self, fileList, treeList, outFileName):
+		self.fileList = fileList
+		self.treeList = treeList
+		self.outFileName = outFileName
+		self.objects = []
+		loadFileList()
+		loadTreeList()
+		loadOutFile()
+
+	def loadFileList(self):
+		inFile = open(self.fileList)
+		self.inputFileList = []
+		for line in inFile:
+			self.inputFileList.append(line)
+		inFile.close()
+
+	def loadTreeList(self):
+		treeFile = open(self.treeList)
+		self.treeNameList = []
+		for line in treeFile:
+			self.treeNameList.append(line)
+		treeFile.close()
+
+	def loadOutFile(self):
+		self.outRootFile = rt.TFile.Open(self.outFileName, 'RECREATE')
+		
+	def getChain(self, tree_name):
+		chain = rt.TChain(tree_name)
+		for fileName in self.inputFileList:
+			chain.Add(fileName)
+		return chain
+	
+	def makeTH1F(self, name, nbinsx, xmin, xmax):
+		hist = rt.TH1F(name, name, nbinsx, xmin, xmax)
+		self.objects.append(hist)
+		return hist
+
+	def makeTH1F(self, name, title, nbinsx, xmin, xmax):
+		hist = rt.TH1F(name, title, nbinsx, xmin, xmax)
+		self.objects.append(hist)
+		return hist
+
+	def makeTH2F(self, name, nbinsx, xmin, xmax, nbinsy, ymin, ymax):
+		hist = rt.TH2F(name, name, nbinsx, xmin, xmax, nbinsy, ymin, ymax)
+		self.objects.append(hist)
+		return hist
+
+	def makeTH2F(self, name, title, nbinsx, xmin, xmax, nbinsy, ymin, ymax):
+		hist = rt.TH2F(name, title, nbinsx, xmin, xmax, nbinsy, ymin, ymax)
+		self.objects.append(hist)
+		return hist
+	
+	def makeTGraph(self, n, x, y):
+		graph = rt.TGraph(n,x,y)
+		self.objects.append(graph)
+		return graph
+	
+	def makeTGraph(self):
+		graph = rt.TGraph()
+		self.objects.append(graph)
+		return graph
+
+	def write(self):
+		self.outRootFile.cd()
+		for thing in self.objects:
+			thing.Write()
+
+	def selfprint(self):
+		print("------------------------")
+		print("Analysing these files:")
+		for line in self.fileList:
+			print(line)
+		print("Using these trees:")
+		for line in self.treeList:
+			print(line)
+		print("And saving output here:")
+		print(self.outFileName)
+		print("------------------------")
+	
+	def run(self):
+		selfprint()
+		loop()
+		write()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,47 @@
+# handler for FAnalysis
+# make sure that all the input files are in order
+# maybe make this a function in the baseAnalysis class?
+# calls the analysis code
+
+
+
+import sys, os
+
+# arguments
+# [0] main.py
+# [1] macro name/path that has the loop function
+# [2] list of input files
+# [3] list of tree names
+# [4] outputFile name/path
+
+for iArg in range(len(sys.argv)):
+	print(str(iArg), sys.argv[iArg])
+
+
+if not os.path.exists(sys.argv[1]):
+	exit("Macro path doesn't exist")
+print("----------")
+print("Macro is:")
+print(sys.argv[1])
+print("----------")
+if not os.path.exists(sys.argv[2]):
+	exit("Input List doesn't exist")
+iFile = open(sys.argv[2])
+print("Input Files are:")
+for line in iFile:
+	print(line[:-1])# don't print new line symbol
+print("----------")
+
+if not os.path.exists(sys.argv[3]):
+	exit("Tree List doesn't exist")
+iTree = open(sys.argv[3])
+print("Tree Names are:")
+for line in iTree:
+	print(line[:-1])# don't print new line symbol
+print("----------")
+
+os.symlink("../$s"%argv[1], "../src/analysisClass.py")
+
+# create instance ofobject analysisClass with string args (inputList, treeList, outFile)
+
+# 
