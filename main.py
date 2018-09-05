9466b9126357bdf36ac8496a4549dfb710571bec
CTFallon <c.fallon@cern.ch>
2018-09-05 12:08:32 -0500

    First Test commit

--- main.py
+++ main.py
@@ -7,7 +7,10 @@
 
 import sys, os
 import analysisBase as ab
-import sys.argv[1][:-3] as tm
+
+os.symlink("../{}".format(sys.argv[1]), "../src/analysisClass.py")
+
+import analysisClass as tm
 
 tm.addLoop()
 
@@ -44,7 +47,6 @@ for line in iTree:
 	print(line[:-1])# don't print new line symbol
 print("----------")
 
-#os.symlink("../{}".format(sys.argv[1]), "../src/analysisClass.py")
 
 analysis = ab.baseClass(sys.argv[2], sys.argv[3], sys.argv[4])
 analysis.run()
