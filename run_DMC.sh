555a1f755823f91a81f4ece13f77fa8ca9ada452
CTFallon <c.fallon@cern.ch>
2019-06-10 15:39:02 -0500

    added name extention optinal argument

--- run_DMC.sh
+++ run_DMC.sh
@@ -2,15 +2,16 @@
 
 
 NAME=$1
+NAMEEXT=${2:-""}
 MACRO="datamc/$NAME.py"
-OUT="$NAME.root"
+OUT="$NAME$NAMEEXT.root"
 
 echo "Submitting jobs for $NAME"
 
 
-#for fileID in Data16 Data17 Data18PRE Data18POST ST16 ST17 ST18PRE ST18POST QCD16 QCD17 QCD18PRE QCD18POST TTJets16 TTJets17 TTJets18PRE TTJets18POST WJets16 WJets17 WJets18PRE WJets18POST ZJets16 ZJets17 ZJets18PRE ZJets18POST
+for fileID in Data16 Data17 Data18PRE Data18POST ST16 ST17 ST18PRE ST18POST QCD16 QCD17 QCD18PRE QCD18POST TTJets16 TTJets17 TTJets18PRE TTJets18POST WJets16 WJets17 WJets18PRE WJets18POST ZJets16 ZJets17 ZJets18PRE ZJets18POST
 #for fileID in base
-for fileID in QCD17 TTJets17 WJets17 ZJets17 base
+#for fileID in QCD17 TTJets17 WJets17 ZJets17 base
 do
 	if [ ! -d "outputs/dmc/$fileID" ]; then
 		mkdir outputs/dmc/$fileID
