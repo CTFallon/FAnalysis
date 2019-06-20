857e8b795cdad0dfbc4006a2b1cd51b6374607af
CTFallon <c.fallon@cern.ch>
2019-06-19 20:17:38 -0500

    PU stuff

--- run_DMC.sh
+++ run_DMC.sh
@@ -9,14 +9,14 @@ OUT="$NAME$NAMEEXT.root"
 echo "Submitting jobs for $NAME"
 
 
-for fileID in Data16 Data17 Data18PRE Data18POST ST16 ST17 ST18PRE ST18POST QCD16 QCD17 QCD18PRE QCD18POST TTJets16 TTJets17 TTJets18PRE TTJets18POST WJets16 WJets17 WJets18PRE WJets18POST ZJets16 ZJets17 ZJets18PRE ZJets18POST
+for fileID in Data16 Data17 Data18PRE Data18POST QCD16 QCD17 QCD18PRE QCD18POST TTJets16 TTJets17 TTJets18PRE TTJets18POST WJets16 WJets17 WJets18PRE WJets18POST ZJets16 ZJets17 ZJets18PRE ZJets18POST
 #for fileID in base
 #for fileID in QCD17 TTJets17 WJets17 ZJets17 base
 do
 	if [ ! -d "outputs/dmc/$fileID" ]; then
 		mkdir outputs/dmc/$fileID
 	fi
-	python main.py $MACRO $fileID inputTree_skims.txt outputs/dmc/$fileID $OUT 2 >& outputs/dmc/$fileID/$NAME.out &
+	python main.py $MACRO $fileID inputTree_skims.txt outputs/dmc/$fileID $OUT 2 >& outputs/dmc/$fileID/$NAME$NAMEEXT.out &
 	echo "Submitted  $fileID"
 	sleep 10
 done
